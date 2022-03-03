93a5d89513e2b5c2c9e8053553948f6eb7a23434
rordonez <robinsonor4@gmail.com>
2022-03-02 20:05:46 -0500

    punto 1

--- punto_1.py
+++ punto_1.py
@@ -5,5 +5,13 @@ Spyder Editor
 This is a temporary script file.
 """
 
-
+donacion = int(input('ingrese el donacion: '))
+telecomunicaciones = donacion * 0.1
+sistemas = donacion * 0.25
+administracion = donacion * 0.35
+contabilidad = donacion * 0.3
+print(f'el presupuesto para telecomunicaciones es: {telecomunicaciones:,}')
+print(f'el presupuesto para sistemas es: {sistemas:,}')
+print(f'el presupuesto para administracion es: {administracion:,}')
+print(f'el presupuesto para contabilidad es: {contabilidad:,}')
 
