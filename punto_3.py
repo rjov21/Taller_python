cfb1a8767804d5b059210712542dbdef070edb63
rordonez <robinsonor4@gmail.com>
2022-03-02 20:13:56 -0500

    punto 3

---
+++ punto_3.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  2 20:08:28 2022
+
+@author: rordonez
+"""
+
+sueldo = int(input('ingrese el sueldo del empleado: '))
+ventas = int(input('ingrese valor de ventas del empleado: '))
+monto_total = sueldo + ventas
+print(f'el monto total del sueldo del empleado es: {monto_total}')
