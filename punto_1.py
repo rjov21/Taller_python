0d504e61da1d820086c3b8efc0de884c88bb7229
rordonez <robinsonor4@gmail.com>
2022-03-02 20:34:51 -0500

    commit final

--- punto_1.py
+++ punto_1.py
@@ -6,12 +6,17 @@ This is a temporary script file.
 """
 
 donacion = int(input('ingrese el donacion: '))
+
 telecomunicaciones = donacion * 0.1
 sistemas = donacion * 0.25
 administracion = donacion * 0.35
 contabilidad = donacion * 0.3
+
 print(f'el presupuesto para telecomunicaciones es: {telecomunicaciones:,}')
+
 print(f'el presupuesto para sistemas es: {sistemas:,}')
+
 print(f'el presupuesto para administracion es: {administracion:,}')
+
 print(f'el presupuesto para contabilidad es: {contabilidad:,}')
 
--- punto_2.py
+++ punto_2.py
@@ -6,12 +6,17 @@ Created on Wed Mar  2 19:59:45 2022
 """
 
 persona1 = int(input('ingrese la cantidad invertida por la persona 1: '))
+
 persona2 = int(input('ingrese la cantidad invertida por la persona 2: '))
+
 persona3 = int(input('ingrese la cantidad invertida por la persona 3: '))
+
 cantidad_total = persona1 + persona2 + persona3
 cantidad_invertida_persona1 = (persona1 * 100) / cantidad_total
 cantidad_invertida_persona2 = (persona2 * 100) / cantidad_total
 cantidad_invertida_persona3 = (persona3 * 100) / cantidad_total
 print(f'el % invertido por persona1 es: {cantidad_invertida_persona1}%')
+
 print(f'el % invertido por persona2 es: {cantidad_invertida_persona2}%')
+
 print(f'el % invertido por persona3 es: {cantidad_invertida_persona3}%')
